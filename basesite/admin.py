4896246115947a2e83f6b78f213d36734e04b340
Jordan <air.jordan.negga@gmail.com>
2021-02-18 12:12:32 +0500

    add all old project

--- basesite/admin.py
+++ basesite/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Genre, Film, MovieShots, Actor, Rating, RatingStar, Reviews, VideoTrailer, Serial, Cartoon, AgeRate
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Film)
+admin.site.register(Serial)
+admin.site.register(Cartoon)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
+admin.site.register(VideoTrailer)
+# admin.site.register(Director)
+# admin.site.register(Scenario)
+admin.site.register(AgeRate)
